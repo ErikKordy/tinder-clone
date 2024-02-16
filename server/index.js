8a92775353dcff6ea656fc17edefde63db644693
Erik K <Kordylasinski.erik@gmail.com>
2024-02-15 19:28:36 -0600

    Created a server and client folder, started working on the backend using express and connecting it to the mongodb cluster db

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const PORT = 8000
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const uri = 'mongodb+srv://kordylasinskierik:bR83zHZllRgycSlE@cluster1.ryop4vl.mongodb.net/?retryWrites=true&w=majority'
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json('Hello app')
+})
+
+app.post('/signup', (req, res) => {
+    res.json('Hello app')
+
+})
+
+app.get('/users', async (req, res) => {
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)
+    } finally {
+        await client.close()
+    }
+})
+
+
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
