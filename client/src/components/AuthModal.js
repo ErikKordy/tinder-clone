b9e191c2d00767592639e1d83c6b7aba30ee6939
Erik K <Kordylasinski.erik@gmail.com>
2024-02-26 20:30:56 -0600

    Set up server side user account creation into mongoDB successfully from the frontend

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,68 +1,77 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const AuthModal= ({setShowModal, isSignUp}) => {
-    const [ email, setEmail ] = useState(null)
-    const [ password, setPassword ] = useState(null)
-    const [ confirmPassword, setConfirmPassword ] = useState(null)
-    const [ error, setError ] = useState(null)
+const AuthModal = ({ setShowModal, isSignUp }) => {
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPassword, setConfirmPassword] = useState(null);
+    const [error, setError] = useState(null);
 
-    console.log(email, password, confirmPassword)
+    let navigate = useNavigate();
 
+    console.log(email, password, confirmPassword);
 
     const handleClick = () => {
-        setShowModal(false)
-    }
+        setShowModal(false);
+    };
 
-    const handleSubmit = (e) => {
-        e.preventDeafault()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         try {
-            if( isSignUp && ( password !== confirmPassword)) {
-                setError('Passwords need to match!')
+            if (isSignUp && ( password !== confirmPassword)) {
+                setError('Passwords need to match!');
+                return;
             }
-            console.log('make a post request to DB')
+            
+            const response = await axios.post('http://localhost:8000/signup', { email, password });
+
+            const success = response.status === 201;
+
+            if (success) navigate('/onboarding');
+            
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
     return (
         <div className="auth-modal">
             <div className="close-icon" onClick={handleClick}>X</div>
-            <h2>{isSignUp ? 'Create Account': 'Log In'}</h2>
+            <h2>{isSignUp ? 'Create Account' : 'Log In'}</h2>
             <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
             <form onSubmit={handleSubmit}>
-            <input
+                <input
                     type="email"
-                    id= "email"
-                    name= "email"
+                    id="email"
+                    name="email"
                     placeholder="email"
                     required={true}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
                     type="password"
-                    id= "password"
-                    name= "password"
+                    id="password"
+                    name="password"
                     placeholder="password"
                     required={true}
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 {isSignUp && <input
                     type="password"
-                    id= "password-confirm"
-                    name= "password-confirm"
+                    id="password-confirm"
+                    name="password-confirm"
                     placeholder="confirm password"
                     required={true}
                     onChange={(e) => setConfirmPassword(e.target.value)}
                 />}
-                <input className='secondary-button' type="submit"/ >
+                <input className='secondary-button' type="submit"/>
                 <p>{error}</p>
             </form>
             <hr/>
             <h2>GET THE APP</h2>
-
         </div>
-    )
-}
+    );
+};
 
-export default AuthModal
+export default AuthModal;
